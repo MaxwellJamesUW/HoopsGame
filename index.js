fda966621bb5eb0f47946b8e9f444952d0440190
MaxwellJamesUW <mxcopp97@uw.edu>
2023-03-16 07:18:37 -0700

    Refined visuals, sound effects

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ import {
     onSnapshot,
     query,
     orderBy,
+    limit,
   } from "firebase/firestore";
 
 // Your web app's Firebase configuration
@@ -40,7 +41,7 @@ let fshots = 0;
 let score = 0;
 let fscore = 0;
 let ended = false;
-let playerName = "Max";
+let playerName = "PlayerOne";
 let ballimg1, ballimg2, ballimg3, ballTired, ballAngry, ballScared;
 let ballAni = 0;
 let countStart;
@@ -54,7 +55,8 @@ let gameState = 0;
 
 let startTime = 0;
 
-let input, button, greeting, title;
+let input, button, greeting, title, bgTitle;
+var sounds = [];
 
 window.preload = () => {
   ballimg1 = loadImage("ball2.png");
@@ -65,6 +67,15 @@ window.preload = () => {
   ballScared = loadImage('tooFast.png');
 
   title = loadImage('HoopsTitle.png');
+  bgTitle = loadImage('bgTitle.png');
+  
+  let oof1 = loadSound('oof.mp3');
+  let oof2 = loadSound('ouch.mp3');
+  let oof4 = loadSound('uhhg.mp3');
+  let oof3 = loadSound('ough.mp3');
+  
+  sounds.push(oof3);
+  
 }
 
 window.setup = () => {
@@ -130,6 +141,7 @@ window.setup = () => {
 window.draw = () => {
   clear();
   background(20);
+  image(bgTitle, cW/5, 2*cH/7, 3*cW/5, cH/6);
 
   let secs = (millis() - startTime) / 1000;
   secs = int(secs);
@@ -138,7 +150,7 @@ window.draw = () => {
   if(gameState == 0){
     fill(245);
     rectMode(CENTER);
-    rect(cW/2, cH/2 + 50, 4*cW/5, 4*cW/5, 8,8,8,8);
+    rect(cW/2, 9*cH/13, cW/2, cW/4, 8,8,8,8);
     image(title, cW/5, 2*cH/7, 3*cW/5, cH/6);
   }
   if(gameState == 1){
@@ -209,6 +221,15 @@ window.draw = () => {
     hoopr.remove();
     newHoop();
   }
+
+
+  //do sound effects on collision with walls or top
+  if(ball.colliding(lwall) || ball.colliding(rwall) || ball.colliding(topper)){
+    let randSound = int(random(sounds.length));
+    sounds[randSound].play();
+  }
+
+
   textAlign(LEFT);
   textSize(40);
   strokeWeight(0);
@@ -228,11 +249,13 @@ window.draw = () => {
         
     }
     displayScores();
+    textAlign(CENTER);
     if (fscore === 1){
-      text('You scored just ' + fscore + " basket\n in 60 seconds!", cW/2, cH/2);
+      text('You scored just ' + fscore + " basket\n in 60 seconds!", cW/2, 3*cH/4);
     } else {
-      text('You scored ' + fscore + " baskets\n in 60 seconds!", cW/2, cH/2);
+      text('You scored ' + fscore + " baskets\n in 60 seconds!", cW/2, 3*cH/4);
     }
+    text('Press Esc to play again', cW/2, cH - 60);
   } else {
     if(gameState == 2){
       textSize(32);
@@ -308,7 +331,7 @@ async function getScores() {
     hiScores = [];
   
     const querySnapshot = await getDocs(
-      query(dbRef, orderBy("score", "desc"))
+      query(dbRef, orderBy("score", "desc"), orderBy("shots"), limit(20))
     );
     querySnapshot.forEach((doc) => {
       let msgData = doc.data();
@@ -321,12 +344,16 @@ async function getScores() {
   
 function displayScores(){
   textAlign(RIGHT);
+  textSize(30);
+  text("HIGH SCORES", cW - 20, 38);
   textSize(20);
-  text("HIGH SCORES", cW - 20, 30);
-  text("Name\tScore\tShots", cW - 20, 50);
-  let yVal = 55;
+  text("Name\tScore \tShots", cW - 20, 62);
+  let yVal = 87;
   hiScores.forEach(ele => {
-    text(ele.uname + '\t\t\t\t' + ele.score + '\t\t\t\t' + ele.shots, cW - 30, yVal += 20);
+    text(ele.shots, cW - 20, yVal);
+    text(ele.score, cW - 90, yVal);
+    text(ele.uname, cW - 150, yVal);
+    yVal += 22
     
   });
 }
@@ -346,21 +373,28 @@ function setupGame(){
   gameState = 0;
 
   input = createInput();
-  input.position(cW/2 - 135, cH - 200);
+  input.size(200)
+  
   input.style('height', '60px');
+  input.style('font-size', '18pt');
 
   button = createButton('start');
-  button.position(input.x + input.width, cH - 200);
+  
   button.style('height', '60px');
   button.style('width','100px');
   button.style('font-size', '20px');
   button.mousePressed(validateName);
 
+  input.position(cW/2 - (input.width + button.width)/2, cH - 260);
+  button.position(input.x + input.width, cH - 260);
+
   greeting = createElement('h2', 'PLAYER NAME:');
   greeting.style('text-align','center');
-  greeting.style('display', 'block');
+  //greeting.style('display', 'block');
+  greeting.style('width','250px');
   greeting.style('font-family','Verdana, Arial, sans-serif');
-  greeting.position(cW/2 - 130, cH/2 + 100);
+  greeting.position(cW/2 - 125, cH - 330);
+  console.log(greeting.width);
 
   noLoop();
 }
--- rollup.config.js
+++ rollup.config.js
@@ -10,7 +10,9 @@ export default {
     copy({
       targets: [{ src: "index.html", dest: "dist" }, { src: "theBall.png", dest: "dist" }, 
         { src: "ball2.png", dest: "dist" }, { src: "ball3.png", dest: "dist" },{ src: "styles.css", dest: "dist" },
-        { src: "angry.png", dest: "dist" },{ src: "tired.png", dest: "dist" },{ src: "tooFast.png", dest: "dist" }],
+        { src: "angry.png", dest: "dist" },{ src: "tired.png", dest: "dist" },{ src: "tooFast.png", dest: "dist" },
+        { src: "HoopsTitle.png", dest: "dist"},{ src: "bgTitle.png", dest: "dist"},
+        { src: "oof.mp3", dest: "dist"},{ src: "ouch.mp3", dest: "dist"},{ src: "ough.mp3", dest: "dist"},{ src: "uhhg.mp3", dest: "dist"},],
     }),
     nodeResolve(),
   ],
